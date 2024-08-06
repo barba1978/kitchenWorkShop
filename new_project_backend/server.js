f2bb80edc0499cbd886822056d979c9ce7ee6453
Andrea Barbieri <barbieri.andrea1978@gmail.com>
2024-08-06 16:25:00 +0300

    nuova array filtrata e inizio di log in e sign up

--- new_project_backend/server.js
+++ new_project_backend/server.js
@@ -26,6 +26,24 @@ app.get('/test', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+//Ruote to get the signup
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+    // Logica per salvare il nuovo utente nel database
+    res.status(201).send('User created');
+  });
+
+ //Ruote to get the Login 
+  app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    // Logica per autenticare l'utente e generare un token JWT
+    const token = generateJWT({ email });
+    res.status(200).json({ token });
+  });
+  
+  
+
+
 
 // Route to get all data from the table
 app.get('/all-data', async (req, res) => {
@@ -37,6 +55,24 @@ app.get('/all-data', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+// post data to the server 
+app.post('/add-data', async (req, res) => {
+    const { title, date, description, hours, guests, price, address, hostname, cellnumber, level, time } = req.body;
+    console.log('req.body',req.body);
+    try {
+      const result = await pool.query(
+        `INSERT INTO workshop1 (title, date, description, hours, guests, price, address, hostname, cellnumber, level, time) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *`,
+        [title, date, description, hours, guests, price, address, hostname, cellnumber, level, time]
+      );
+      console.log(result);
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
 
 // Start the server
 app.listen(port, () => {
